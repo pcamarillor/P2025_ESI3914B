807a60404307d9f97eac5d942fbfea95d58fe71c
Alberto <alberto.renteriac@iteso.mx>
2025-02-18 08:45:50 -0600

    spark_utils.py was implemented

--- spark_cluster/notebooks/lib/equipo/spark_utils.py
+++ spark_cluster/notebooks/lib/equipo/spark_utils.py
@@ -1,6 +1,24 @@
-from pyspark.sql.types import StructType
+from pyspark.sql.types import *
+
+
+types = {
+"StringType": StringType(),
+"IntegerType": IntegerType(),
+"LongType": LongType(),
+"ShortType": ShortType(),
+"DoubleType": DoubleType(),
+"FloatType": FloatType(),
+"BooleanType": BooleanType(),
+"DateType": DateType(),
+"TimestampType": TimestampType(),
+"BinaryType": BinaryType(),
+"ArrayType": ArrayType,
+"MapType" : MapType,
+"StructType": StructType
+}
 
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
-        raise NotImplementedError("Not implemented yet")
+        fields = [StructField(name, types[type_name], True) for name, type_name in columns_info]
+        return StructType(fields)
