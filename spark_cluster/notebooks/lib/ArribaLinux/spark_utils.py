93de2215c85fb1226dd4bc67b0c1afae187f1ef7
Tiz . <tirzah03pb@gmail.com>
2025-02-18 08:45:24 -0600

    Librería y ejemplo de funcionamiento

--- spark_cluster/notebooks/lib/ArribaLinux/spark_utils.py
+++ spark_cluster/notebooks/lib/ArribaLinux/spark_utils.py
@@ -1,6 +1,18 @@
-from pyspark.sql.types import StructType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, BooleanType, DateType
 
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
-        raise NotImplementedError("Not implemented yet")
+        types_dic = {
+            "StringType": StringType(),
+            "IntegerType": IntegerType(),
+            "FloatType": FloatType(),
+            "BooleanType": BooleanType(),
+            "DateType": DateType()
+        }
+
+        fields = [StructField(data, types_dic[data_type], True) for data, data_type in columns_info]
+    
+        return StructType(fields)
+        
+        
