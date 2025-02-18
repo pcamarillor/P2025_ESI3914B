806c01c5771356cfc77e31ec609a136aa063955a
EduardoHGomez <hurtado.josegomez@gmail.com>
2025-02-18 08:30:04 -0600

    JJAE clase 09

--- spark_cluster/notebooks/lib/JJAE/spark_utils.py
+++ spark_cluster/notebooks/lib/JJAE/spark_utils.py
@@ -1,6 +1,41 @@
+import findspark
 from pyspark.sql.types import StructType
+findspark.init()
 
 class SparkUtils:
+
+    self.types = {
+        "StringType": StringType(),
+        "IntegerType": IntegerType(),
+        "LongType": LongType(),
+        "ShortType": ShortType(),
+        "DoubleType": DoubleType(),
+        "FloatType": FloatType(),
+        "BooleanType": BooleanType(),
+        "DateType": DateType(),
+        "TimestampType": TimestampType(),
+        "BinaryType": BinaryType()
+    }
+
     @staticmethod
     def generate_schema(columns_info) -> StructType:
-        raise NotImplementedError("Not implemented yet")
+        struct = []
+        for column in columns_info:
+            struct.append(StructField(column[0], self.types[column[1]], True))
+
+        return StructType(struct)
+
+
+# Test
+sample_data = [
+    ("id", "IntegerType" ),
+    ("name", "StringType" ),
+    ("age", "IntegerType" ),
+    ("mail", "StringType" ),
+    ("phone", "StringType" ),
+    ("created_at", "TimestampType"),
+    ("height", "FloatType"),
+]
+
+schema = SparkUtils.generate_schema(sample_data)
+print(schema)
