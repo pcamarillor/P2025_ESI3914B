b0ffce1a1611dfe2a1cc611a5063848946507a80
José Mendoza <joseantoniomendozanavarro@gmail.com>
2025-02-18 11:55:04 -0600

    test implemented

--- spark_cluster/notebooks/lib/JJAE/spark_utils.py
+++ spark_cluster/notebooks/lib/JJAE/spark_utils.py
@@ -1,42 +1,27 @@
 import findspark
-from pyspark.sql.types import StructType
-findspark.init()
+from pyspark.sql.types import StructType, StructField , StringType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, TimestampType, BinaryType
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum, avg
 
 class SparkUtils:
 
-    self.types = {
-        "StringType": StringType(),
-        "IntegerType": IntegerType(),
-        "LongType": LongType(),
-        "ShortType": ShortType(),
-        "DoubleType": DoubleType(),
-        "FloatType": FloatType(),
-        "BooleanType": BooleanType(),
-        "DateType": DateType(),
-        "TimestampType": TimestampType(),
-        "BinaryType": BinaryType()
-    }
-
     @staticmethod
     def generate_schema(columns_info) -> StructType:
+        types = {
+            "StringType": StringType(),
+            "IntegerType": IntegerType(),
+            "LongType": LongType(),
+            "ShortType": ShortType(),
+            "DoubleType": DoubleType(),
+            "FloatType": FloatType(),
+            "BooleanType": BooleanType(),
+            "DateType": DateType(),
+            "TimestampType": TimestampType(),
+            "BinaryType": BinaryType()
+        }
+
         struct = []
         for column in columns_info:
-            struct.append(StructField(column[0], self.types[column[1]], True))
+            struct.append(StructField(column[0], types[column[1]], True))
 
         return StructType(struct)
-
-
-# Test
-sample_data = [
-    ("id", "IntegerType" ),
-    ("name", "StringType" ),
-    ("age", "IntegerType" ),
-    ("mail", "StringType" ),
-    ("phone", "StringType" ),
-    ("created_at", "TimestampType"),
-    ("height", "FloatType"),
-    ("weight", "FloatType"),
-]
-
-schema = SparkUtils.generate_schema(sample_data)
-print(schema)
