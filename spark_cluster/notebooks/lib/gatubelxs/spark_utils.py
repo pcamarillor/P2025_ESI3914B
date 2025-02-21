75628d85c7dc9804f815f108c1180c88a3eb6ac2
SamQuintero <samabyq@gmail.com>
2025-02-21 08:09:53 -0600

    Notebook creation and fix in lib

--- spark_cluster/notebooks/lib/gatubelxs/spark_utils.py
+++ spark_cluster/notebooks/lib/gatubelxs/spark_utils.py
@@ -1,15 +1,21 @@
-from pyspark.sql.types import StructType ,StructField, StringType, IntegerType,FloatType,DateType,BooleanType,DoubleType
+from pyspark.sql.types import StructType ,StructField, StringType, IntegerType,FloatType,DateType,BooleanType,DoubleType,ByteType,TimestampType,ShortType,BinaryType,LongType,DecimalType
 
 class SparkUtils:
     @staticmethod
     def generate_schema(columns_info) -> StructType:
         struct_type_dict = {
-                            "StringType": StringType(),
-                            "FloatType": FloatType(),
-                            "IntegerType": IntegerType(),
-                            "BooleanType": BooleanType(),
-                            "DateType": DateType(),
-                            "DoubleType": DoubleType()
+                            "byte":ByteType(),
+                            "string": StringType(),
+                            "float": FloatType(),
+                            "integer": IntegerType(),
+                            "bool": BooleanType(),
+                            "date": DateType(),
+                            "double": DoubleType(),
+                            "timestamp":TimestampType(),
+                            "short": ShortType(),
+                            "binary":BinaryType(),
+                            "long": LongType(),
+                            "decimal": DecimalType()
                         }
         struct_list=[]
         for col in columns_info:
