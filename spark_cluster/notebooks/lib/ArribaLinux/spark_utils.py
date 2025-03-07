0152888397bf3ed3011f0bbf1738830bf2834f41
Tiz . <tirzah03pb@gmail.com>
2025-03-06 20:41:07 -0600

    Corrección de laboratorio 4

--- spark_cluster/notebooks/lib/ArribaLinux/spark_utils.py
+++ spark_cluster/notebooks/lib/ArribaLinux/spark_utils.py
@@ -1,4 +1,4 @@
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, BooleanType, DateType, LongType, ShortType, DoubleType, TimestampType, BinaryType, ByteType, DecimalType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, BooleanType, DateType, LongType, ShortType, DoubleType, TimestampType, BinaryType, ByteType, DecimalType, ArrayType, DataType, MapType, NullType, CharType, VarcharType, DayTimeIntervalType, YearMonthIntervalType
 
 class SparkUtils:
     @staticmethod
@@ -15,7 +15,15 @@ class SparkUtils:
             "timestamp": TimestampType(),
             "binary": BinaryType(),
             "byte": ByteType(),
-            "decimal": DecimalType()
+            "decimal": DecimalType(),
+            "array": lambda element_type: ArrayType(element_type),
+            "data": DataType(),
+            "map": lambda key_type, value_type: MapType(key_type, value_type),
+            "null": NullType(),
+            "char": CharType(),
+            "varchar": VarcharType(),
+            "dayTimeInterval": DayTimeIntervalType(),
+            "yearMonthInterval": YearMonthIntervalType()
         }
 
         fields = [StructField(data, types_dic[data_type], True) for data, data_type in columns_info]
