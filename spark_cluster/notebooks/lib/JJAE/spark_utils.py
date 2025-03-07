f7cf21e921e4688261629f90256047df47ad30eb
Andres <Andresblancocontact@gmail.com>
2025-03-07 07:21:06 -0600

    Fixed lab

---
+++ spark_cluster/notebooks/lib/JJAE/spark_utils.py
@@ -0,0 +1,25 @@
+from pyspark.sql.types import (
+    StructType, StructField, StringType, IntegerType, LongType, ShortType, 
+    DoubleType, FloatType, BooleanType, DateType, TimestampType, BinaryType
+)
+
+class SparkUtils:
+    @staticmethod
+    def generate_schema(columns_info) -> StructType:
+        # Store all types in a dictionary
+        types_dic = {
+            "StringType": StringType(),
+            "IntegerType": IntegerType(),
+            "LongType": LongType(),
+            "ShortType": ShortType(),
+            "DoubleType": DoubleType(),
+            "FloatType": FloatType(),
+            "BooleanType": BooleanType(),
+            "DateType": DateType(),
+            "TimestampType": TimestampType(),
+            "BinaryType": BinaryType()
+        }
+        
+        # Create schema
+        schema = [StructField(column, types_dic[data_type], True) for column, data_type in columns_info]
+        return StructType(schema)
