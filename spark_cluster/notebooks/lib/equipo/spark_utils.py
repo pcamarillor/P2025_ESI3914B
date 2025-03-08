ad0000849bc2f975e0fad493fc73979c3b61c9ca
Yael <yael.rodriguez@iteso.mx>
2025-03-07 18:36:26 -0600

    Pruebenlo

--- spark_cluster/notebooks/lib/equipo/spark_utils.py
+++ spark_cluster/notebooks/lib/equipo/spark_utils.py
@@ -1,22 +1,31 @@
 from pyspark.sql.types import *
 
-
-types = {
-    "string": StringType(),
-    "integer": IntegerType(),
-    "long": LongType(),
-    "short": ShortType(),
-    "double": DoubleType(),
-    "float": FloatType(),
-    "boolean": BooleanType(),
-    "date": DateType(),
-    "timestamp": TimestampType(),
-    "binary": BinaryType(),
-}
-
-
 class SparkUtils:
+    types = {
+        "string": StringType(),
+        "integer": IntegerType(),
+        "long": LongType(),
+        "short": ShortType(),
+        "double": DoubleType(),
+        "float": FloatType(),
+        "boolean": BooleanType(),
+        "date": DateType(),
+        "timestamp": TimestampType(),
+        "binary": BinaryType(),
+    }
+    
     @staticmethod
     def generate_schema(columns_info) -> StructType:
-        fields = [StructField(name, types[type_name], True) for name, type_name in columns_info]
+        fields = [StructField(name, SparkUtils.types[type_name], True) for name, type_name in columns_info]
         return StructType(fields)
+    
+    @staticmethod
+    def clean_df(df):
+        return df.dropna()
+    
+    @staticmethod
+    def write_df(df, partition, directory):
+        df.write \
+        .mode("overwrite") \
+        .partitionBy(partition) \
+        .parquet(directory)
