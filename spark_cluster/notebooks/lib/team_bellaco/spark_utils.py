9b8e689ec5b1289d606ddff5a4ce19bcf3c4e21a
Angel_Cortes(Mr.js) <is726442@iteso.mx>
2025-03-08 14:12:23 -0600

    Lo que llevamos hasta ahora pero no esta terminado, falta los valores nullos de date_added

--- spark_cluster/notebooks/lib/team_bellaco/spark_utils.py
+++ spark_cluster/notebooks/lib/team_bellaco/spark_utils.py
@@ -46,3 +46,51 @@ class SparkUtils:
             struct.append(StructField(nombre, dato, True))
 
         return StructType(struct)
+    
+    @staticmethod
+    def clean_df(df):
+    # Registrar el DataFrame original como vista temporal
+        df.createOrReplaceTempView("netflix_raw")
+    
+     # Crear una tabla temporal con el esquema deseado
+        spark.sql("""
+        CREATE OR REPLACE TEMPORARY VIEW netflix_clean AS
+        SELECT
+            show_id,
+            type,
+            title,
+            COALESCE(director, 'Not Given') AS director NOT NULL,
+            COALESCE(country, 'Unknown') AS country NOT NULL,
+            COALESCE(rating, 'Not Rated') AS rating NOT NULL,
+            COALESCE(duration, '0 min') AS duration NOT NULL,
+            COALESCE(listed_in, 'Uncategorized') AS listed_in NOT NULL,
+            COALESCE(release_year, 0) AS release_year NOT NULL,
+            date_added
+        FROM netflix_raw
+        """)
+    
+    # Recuperar el DataFrame limpio con el esquema correcto
+        clean_df = spark.table("netflix_clean")
+    
+    # Mostrar el recuento de valores nulos después de la limpieza
+        print("Number of nulls after cleaning:")
+        clean_df.select([sum(col(c).isNull().cast("int")).alias(c) for c in clean_df.columns]).show()
+    
+        return clean_df
+    
+    
+    @staticmethod
+    def write_df(df, path, format="parquet", mode="overwrite", partition_by=None):
+        writer = df.write.format(format).mode(mode)
+        
+        # Si se especifica partición, aplicarla
+        if partition_by:
+            writer = writer.partitionBy(partition_by)
+            partition_info = f" particionado por {partition_by}"
+        else:
+            partition_info = ""
+            
+        # Guardar el DataFrame
+        writer.save(path)
+        
+        print(f"DataFrame escrito exitosamente en {path} en formato {format}{partition_info}")
