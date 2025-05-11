1210a08bc1a92d641ccda750a3e99f96f4426479
TBG <albanesemarco66@gmail.com>
2025-05-10 20:52:22 -0600

    Data generator for producers

---
+++ spark_cluster/notebooks/lib/equipo_mcqueen/website_activity.py
@@ -0,0 +1,20 @@
+from faker import Faker
+import random
+from datetime import datetime
+
+fake = Faker()
+
+def generate_website_event():
+    return {
+        "event_id": fake.uuid4(),
+        "user_id": fake.uuid4(),
+        "page_url": fake.uri(),
+        "timestamp": datetime.utcnow().isoformat() + "Z",
+        "action_type": random.choice(['view', 'click', 'scroll']),
+        "browser": fake.user_agent(),
+        "timezone": fake.timezone(),
+        "device_type": random.choice(['mobile', 'desktop', 'tablet']),
+        "click_coordinates": (random.randint(0, 1920), random.randint(0, 1080)),
+        "session_id": fake.uuid4(),
+        "ip_address": fake.ipv4()
+    }
