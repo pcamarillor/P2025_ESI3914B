e35f07acec21393ad278b3fc20d291b60598b7a1
TBG <albanesemarco66@gmail.com>
2025-05-13 02:19:00 -0600

    Nearly there
    Co-Authored-By: VicenteSanche <130470339+VicenteSanche@users.noreply.github.com>

--- spark_cluster/notebooks/lib/equipo_mcqueen/website_activity.py
+++ spark_cluster/notebooks/lib/equipo_mcqueen/website_activity.py
@@ -14,7 +14,8 @@ def generate_website_event():
         "browser": fake.user_agent(),
         "timezone": fake.timezone(),
         "device_type": random.choice(['mobile', 'desktop', 'tablet']),
-        "click_coordinates": (random.randint(0, 1920), random.randint(0, 1080)),
+        "click_coordinates_x": random.randint(0, 1920),
+        "click_coordinates_y": random.randint(0, 1080),
         "session_id": fake.uuid4(),
         "ip_address": fake.ipv4()
     }
